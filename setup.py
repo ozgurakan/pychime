d1c9478bb5bb8e9b6cde0b76d6cd1a3934417a13
Oz Akan <oz@akan.me>
2017-12-04 12:10:12 -0500

    changed email

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='pychime',
-    version='0.1',
+    version='0.1.1',
     description='A library to use Amazon Chime Web Hooks',
     author='Oz Akan',
-    author_email='oz@akan.me',
+    author_email='code@akan.me',
     url='https://github.com/ozgurakan/pychime',
     keywords=['rss', 'feed', 'aws', 'dynamodb'],
     classifiers=[],
